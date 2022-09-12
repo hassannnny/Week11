2d0b3539b9e2edd2fbdf842175ef0de7eaaf3d13
Hassan Javed <90116015+hassannnny@users.noreply.github.com>
2022-09-12 10:01:59 -0500

    Updated Homepage

--- Components/Home.js
+++ Components/Home.js
@@ -5,6 +5,12 @@ const Home = () => {
 
         <div className="parent">
 
+            <div className="slogan">
+                Envision. <br></br>
+                Create. <br></br>
+                Transform.
+            </div>
+
             <img className="desktop" src={require("../img/monitor.png")}/>
             <img className="keyboard"src={require("../img/keyboard3.png")} width="300" height="50"/>
            
