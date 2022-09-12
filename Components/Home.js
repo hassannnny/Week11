3ef99f5a15abd67505639ea539db9c5c1b0abe5c
Hassan Javed <90116015+hassannnny@users.noreply.github.com>
2022-09-12 09:33:52 -0500

    Components w/ updated Homepage, Login, and Sign Up

---
+++ Components/Home.js
@@ -0,0 +1,17 @@
+
+const Home = () => { 
+    return (
+
+
+        <div className="parent">
+
+            <img className="desktop" src={require("../img/monitor.png")}/>
+            <img className="keyboard"src={require("../img/keyboard3.png")} width="300" height="50"/>
+           
+        </div>
+
+    )
+}
+
+
+export default Home
---
+++ Components/NavBar.js
@@ -0,0 +1,21 @@
+
+
+const NavBar = () => { 
+    return (
+
+    <ul>
+        <li>
+            <a className="waterMark" href="/">Hassan&Devon Inc</a>
+            <a className="templates">Templates</a>
+            <a className="login" href="/login">Log In</a>
+            <a className="register" href="/signup">Sign Up</a>
+        </li>
+    </ul>
+        
+
+
+    )
+}
+
+
+export default NavBar
